bf348c800c8037fc9e2b7d9f1effdcd342bf904b
Chris Stoddart <cstoddart@rocketmail.com>
2018-09-10 00:22:22 -0500

    square hover effect

--- src/components/ui/Square/Square.js
+++ src/components/ui/Square/Square.js
@@ -2,7 +2,15 @@ import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
 
 import { distanceFromCenter } from '../../../utilities';
-import { StyledSquare, MaskedImage } from './SquareStyles';
+import { 
+  SquareContainer,
+  StyledSquare,
+  MaskedImage,
+  Top,
+  Left,
+  Right,
+  Bottom,
+} from './SquareStyles';
 
 export class Square extends Component {
   squareRef = createRef();
@@ -19,20 +27,30 @@ export class Square extends Component {
   render() {
     return (
       <div ref={this.squareRef}>
-        <StyledSquare
+        <SquareContainer 
           size={this.props.size}
-          color={this.props.color}
-          direction={this.props.direction}
           depth={this.props.depth}
           offset={this.state.centerOffset}
         >
-          {this.props.image &&
-            <MaskedImage
-              size={this.props.size}
-              image={this.props.image}
-            />
-          }
-        </StyledSquare>
+          <Top size={this.props.size} />
+          <Left size={this.props.size} />
+          <Right size={this.props.size} />
+          <Bottom size={this.props.size} />
+          <StyledSquare
+            className="square"
+            size={this.props.size}
+            color={this.props.color}
+            direction={this.props.direction}
+            depth={this.props.depth}
+          >
+            {this.props.image &&
+              <MaskedImage
+                size={this.props.size}
+                image={this.props.image}
+              />
+            }
+          </StyledSquare>
+        </SquareContainer>          
       </div>
     );
   } 
--- src/components/ui/Square/SquareStyles.js
+++ src/components/ui/Square/SquareStyles.js
@@ -2,6 +2,25 @@ import styled from 'styled-components';
 
 import { PERSPECTIVE, BLUE, GREEN } from '../../../constants';
 
+export const SquareContainer = styled.div`
+  width: ${props => `${props.size}px`};
+  height: ${props => `${props.size}px`};
+  border-radius: ${props => `${(props.size / 8) + 15}px`};
+  transform: rotate(45deg);
+  transform: ${props => `
+    translateZ(${props.depth}px)
+    translateX(${-1 * (props.depth / PERSPECTIVE) * props.offset}px)
+    scale(${-1 * props.depth / PERSPECTIVE + 1})
+    rotate(45deg)
+  `};
+
+  @media (max-width:980px) {
+    width: ${props => `${props.size / 2}px`};
+    height: ${props => `${props.size / 2}px`};
+    border-radius: ${props => `${(props.size / 16) + 10}px`};
+  }
+`;
+
 export const StyledSquare = styled.div`
   display: inline-block;
   transition: 1s ease;
@@ -22,16 +41,6 @@ export const StyledSquare = styled.div`
       ${(props.color === 'blue' && `${BLUE}00`) ||
       (props.color === 'green' && `${GREEN}00`)}
     )
-  `};
-  transform: rotate(45deg);
-  transform: ${props => `
-    translateZ(
-      ${props.depth}px
-    ) translateX(
-      ${-1 * (props.depth / PERSPECTIVE) * props.offset}px
-    ) scale(
-      ${-1 * props.depth / PERSPECTIVE + 1}
-    ) rotate(45deg)
   `};
 
   @media (max-width:980px) {
@@ -57,5 +66,69 @@ export const MaskedImage = styled.div`
     height: ${props => `${props.size / 2}px`};
     top: ${props => `${props.size / 18}px`};
     left: ${props => `${props.size / 18}px`};
+  }
+`;
+
+export const Top = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 50%;
+  height: 50%;
+  z-index: 500;
+  
+  &:hover ~ .square { 
+    transform: ${props => `
+      rotateX(${200 / props.size}deg)
+      rotateY(-${200 / props.size}deg);
+    `}
+  }
+`;
+
+export const Left = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 50%;
+  height: 50%;
+  z-index: 500;
+  
+  &:hover ~ .square { 
+    transform: ${props => `
+      rotateX(-${200 / props.size}deg)
+      rotateY(-${200 / props.size}deg);
+    `}
+  }
+`;
+
+export const Right = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 50%;
+  height: 50%;
+  z-index: 500;
+  
+  &:hover ~ .square { 
+    transform: ${props => `
+      rotateX(${200 / props.size}deg)
+      rotateY(${200 / props.size}deg);
+    `}
+  }
+`;
+
+export const Bottom = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 50%;
+  height: 50%;
+  z-index: 500;
+  
+  &:hover ~ .square { 
+    transform: ${props => `
+      rotateX(-${200 / props.size}deg)
+      rotateY(${200 / props.size}deg);
+    `}
   }
 `;
